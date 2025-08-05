5bca349168ef2503f1268e812ff5a4031ce480d3
Radhika210106 <radhika9301@mountzion.ac.in>
2025-08-05 12:31:08 +0530

    Create statement.java

---
+++ statement.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        int  x =18;
+        int  y = 22;
+        if (x<y)
+        {
+          System.out.println("eligible");
+            
+        }
+           else
+           {
+               System.out.println("not eligible");
+           }
+    }
+}
